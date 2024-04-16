7fded19ba0db219621a590f63b8f51d196b80eb4
Matthew Foyle <mfoyle93@gmail.com>
2024-04-16 12:29:49 +0200

    chore: packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import {
   DynamicContextProvider,
-  SolanaWalletConnectors,
   EthereumWalletConnectors,
-  FlowWalletConnectors,
-  CosmosWalletConnectors,
-  BloctoEvmWalletConnectors,
-  AlgorandWalletConnectors,
-  StarknetWalletConnectors,
-  MagicWalletConnectors,
 } from "../lib/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,16 +23,7 @@ export default function RootLayout({
       <DynamicContextProvider
         settings={{
           environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-          walletConnectors: [
-            EthereumWalletConnectors,
-            FlowWalletConnectors,
-            CosmosWalletConnectors,
-            BloctoEvmWalletConnectors,
-            AlgorandWalletConnectors,
-            StarknetWalletConnectors,
-            MagicWalletConnectors,
-            SolanaWalletConnectors,
-          ],
+          walletConnectors: [EthereumWalletConnectors],
         }}
       >
         <body className={inter.className}>{children}</body>
